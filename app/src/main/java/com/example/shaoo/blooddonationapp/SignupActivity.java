b10e0ffef421ef9dcafded2133a05cabe6b0c524
Shaoor Munir <shaoormunir@outlook.com>
2017-09-08 18:44:28 +0500

    added a date picker

--- app/src/main/java/com/example/shaoo/blooddonationapp/SignupActivity.java
+++ app/src/main/java/com/example/shaoo/blooddonationapp/SignupActivity.java
@@ -1,10 +1,16 @@
 package com.example.shaoo.blooddonationapp;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -20,6 +26,37 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"Clicked back button",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final Calendar c = Calendar.getInstance();
+        final int mYear = c.get(Calendar.YEAR);
+        final int mMonth = c.get(Calendar.MONTH);
+        final int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        final EditText txtDate = (EditText) findViewById(R.id.dateofBirth);
+        txtDate.setTag(txtDate.getKeyListener());
+        txtDate.setKeyListener(null);
+
+        txtDate.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(SignupActivity.this,
+                            new DatePickerDialog.OnDateSetListener() {
+                                @Override
+                                public void onDateSet(DatePicker view, int year,
+                                                      int monthOfYear, int dayOfMonth) {
+
+                                    txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                                    Toast.makeText(SignupActivity.this, "Date picker clicked", Toast.LENGTH_SHORT);
+
+                                }
+                            }, mYear, mMonth, mDay);
+                    datePickerDialog.show();
+                }
+                return false;
             }
         });
     }
